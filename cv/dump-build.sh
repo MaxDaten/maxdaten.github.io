84466090a6993df0fe1b58104b19e4530a157ee4
Jan-Philip Loos <maxdaten@gmail.com>
2015-03-02 20:12:05 +0100

    update new cv

---
+++ cv/dump-build.sh
@@ -0,0 +1,2 @@
+pandoc -H ./tex/base.tex -o ./2015-03-02-curriculum-vitae.pdf ./2015-03-02-curriculum-vitae.md
+pandoc -s -t html5 -H ./css/cv.css -o ./2015-03-02-curriculum-vitae.html ./2015-03-02-curriculum-vitae.md
