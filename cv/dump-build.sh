48c5b5b1c80da127d94654a12767627bf7296b56
Jan-Philip Loos <maxdaten@gmail.com>
2016-04-24 22:35:59 +0200

    Shrink to one version
    this is what vcs is for ;)

--- cv/dump-build.sh
+++ cv/dump-build.sh
@@ -1,2 +1,2 @@
-pandoc -H ./tex/base.tex -o ./2015-03-02-curriculum-vitae.pdf ./2015-03-02-curriculum-vitae.md
-pandoc -s -t html5 -H ./css/cv.css -o ./2015-03-02-curriculum-vitae.html ./2015-03-02-curriculum-vitae.md
+pandoc -H ./tex/base.tex -o ./curriculum-vitae.pdf ./curriculum-vitae.md
+pandoc -s -t html5 -H ./css/cv.css -o ./curriculum-vitae.html ./curriculum-vitae.md
