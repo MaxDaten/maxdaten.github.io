344e548d27e680fae57319476cda5582c77f3a92
Jan-Philip Loos <maxdaten@gmail.com>
2020-03-06 22:56:22 +0100

    Nixify project

---
+++ default.nix
@@ -0,0 +1,21 @@
+let
+  pkgs = import <nixpkgs> {};
+
+  texlive = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+    scheme-small;
+  };
+in {
+  inherit pkgs;
+  inherit texlive;
+
+  pages = pkgs.stdenv.mkDerivation {
+    name = "cv.maxdaten.io";
+    phases = [];
+    buildInputs = [
+      texlive
+      pkgs.pandoc
+      pkgs.watchexec
+    ];
+  };
+}
